3acaa79c383caf12a3789639178f93bc1b45c23e
liuwenbin <liuwb@koal.com>
2017-09-01 18:45:03 +0800

    only init mutex when first fopen "/dev/ashmem"

--- jni/com_example_ashmem_JNIInterface.c
+++ jni/com_example_ashmem_JNIInterface.c
@@ -18,7 +18,6 @@
 	}
 
 int g_fd2ashmem;
-pthread_mutex_t* g_mutex;
 
 JNIEXPORT jint JNICALL Java_com_example_ashmem_JNIInterface_initAndGetFd2Ashmem
   (JNIEnv *env, jclass thiz){
@@ -26,53 +25,46 @@ JNIEXPORT jint JNICALL Java_com_example_ashmem_JNIInterface_initAndGetFd2Ashmem
     jint fd = open("/dev/ashmem",O_RDWR);
     ioctl(fd,ASHMEM_SET_NAME,"MyAshmemName");
     ioctl(fd,ASHMEM_SET_SIZE,4096);
-    g_fd2ashmem = fd;
-    LOGD("initAndGetFd2Ashmem open fd:%d",g_fd2ashmem);
-    return fd;
-}
 
-
-//创建共享的mutex
-void init_mutex(int fd)
-{
-	int ret,zero=0;
-
-	g_mutex=(pthread_mutex_t*)mmap(NULL, sizeof(pthread_mutex_t), PROT_READ|PROT_WRITE, MAP_SHARED,fd==0?g_fd2ashmem:fd,0);
-
-	if( MAP_FAILED==g_mutex )
+    pthread_mutex_t *p_mutex=(pthread_mutex_t*)mmap(NULL, sizeof(pthread_mutex_t), PROT_READ|PROT_WRITE, MAP_SHARED,fd,0);
+	if( MAP_FAILED==p_mutex )
 	{
-		LOGD("mmap:%d",errno);
-		LOGD("strerror:%s",strerror(errno));
-		return;
-	}
-
-	if(fd!=0){
-		// 与Linux下表现一样，二次初始化会导致共享锁失败
-		LOGD("RemoteService don`t init_mutex ");
+		LOGD("mmap:[%d](%s)",errno,strerror(errno));
 		return;
 	}
 
 	pthread_mutexattr_t attr;
 	pthread_mutexattr_init(&attr);
-	ret=pthread_mutexattr_setpshared(&attr,PTHREAD_PROCESS_SHARED);
+	int ret=pthread_mutexattr_setpshared(&attr,PTHREAD_PROCESS_SHARED);
 	if( ret!=0 )
 	{
 		LOGD("init_mutex pthread_mutexattr_setpshared");
 		return;
 	}
-	pthread_mutex_init(g_mutex, &attr);
+	pthread_mutex_init(p_mutex, &attr);
+
+	munmap(p_mutex, sizeof(pthread_mutex_t));
+
+    g_fd2ashmem = fd;
+    LOGD("initAndGetFd2Ashmem open fd:%d",g_fd2ashmem);
+    return fd;
 }
 
 JNIEXPORT void JNICALL Java_com_example_ashmem_JNIInterface_doOperaNow
   (JNIEnv *env, jclass thiz){
-	init_mutex(0);
-	int ret=pthread_mutex_lock(g_mutex);
-	if( ret!=0 )
+	pthread_mutex_t *p_mutex=(pthread_mutex_t*)mmap(NULL, sizeof(pthread_mutex_t), PROT_READ|PROT_WRITE, MAP_SHARED,g_fd2ashmem,0);
+	if( MAP_FAILED==p_mutex )
 	{
-		LOGD("child pthread_mutex_lock");
+		LOGD("mmap:[%d](%s)",errno,strerror(errno));
 		return;
 	}
-	//测试是否能够阻止父进程的写入
+	int ret=pthread_mutex_lock(p_mutex);
+	if( ret!=0 )
+	{
+		LOGD("doOperaNow pthread_mutex_lock");
+		return;
+	}
+	//测试是否能够阻止其他进程的进入
 	sleep(2);
 	LOGD("this is process a.doing AAAAAAAA");
 	sleep(1);
@@ -82,22 +74,26 @@ JNIEXPORT void JNICALL Java_com_example_ashmem_JNIInterface_doOperaNow
 	sleep(1);
 	LOGD("this is process a.doing DDDDDDDD");
 
-	ret=pthread_mutex_unlock(g_mutex);
+	ret=pthread_mutex_unlock(p_mutex);
 	if( ret!=0 )
 	{
 		LOGD("child pthread_mutex_unlock");
 	}
 
-    munmap(g_mutex, sizeof(pthread_mutex_t));
+    munmap(p_mutex, sizeof(pthread_mutex_t));
 }
 
 
 JNIEXPORT void JNICALL Java_com_example_ashmem_JNIInterface_doOperaLater
   (JNIEnv *env, jclass thiz, jint fd){
-
-	init_mutex(fd);
-	sleep(2);// 保证process a先行
-	int ret=pthread_mutex_lock(g_mutex);
+	pthread_mutex_t *p_mutex=(pthread_mutex_t*)mmap(NULL, sizeof(pthread_mutex_t), PROT_READ|PROT_WRITE, MAP_SHARED,fd,0);
+	if( MAP_FAILED==p_mutex )
+	{
+		LOGD("mmap:[%d](%s)",errno,strerror(errno));
+		return;
+	}
+	sleep(2);// 保证主进程先行
+	int ret=pthread_mutex_lock(p_mutex);
 	if( ret!=0 )
 	{
 		LOGD("child pthread_mutex_lock");
@@ -110,13 +106,13 @@ JNIEXPORT void JNICALL Java_com_example_ashmem_JNIInterface_doOperaLater
 	sleep(1);
 	LOGD("this is process b.doing DDDDDDDD");
 
-	ret=pthread_mutex_unlock(g_mutex);
+	ret=pthread_mutex_unlock(p_mutex);
 	if( ret!=0 )
 	{
 		LOGD("child pthread_mutex_unlock");
 	}
 
-	munmap(g_mutex, sizeof(pthread_mutex_t));
+	munmap(p_mutex, sizeof(pthread_mutex_t));
 
 }
 
